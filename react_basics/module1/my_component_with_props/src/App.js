e9eccf8da949373ac4ba6f26be20c4622e794d98
Karlijn Zaanen <karlijn_zaanen@live.nl>
2024-07-10 14:31:42 +0200

    Lab on props

---
+++ react_basics/module1/my_component_with_props/src/App.js
@@ -0,0 +1,13 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Bob" />
+            <Heading firstName="Anna" />
+        </div>
+    );
+};
+
+export default App;
+
