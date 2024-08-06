d561e5281b2ad31860077b1d9cf4f16aeb6334f4
Karlijn Zaanen <karlijn_zaanen@live.nl>
2024-08-06 16:05:09 +0200

    sampleProject expo app

---
+++ react_native/module1/script_to_run.sh
@@ -0,0 +1,6 @@
+brew install node
+brew install watchman
+npx create-expo-app sampleProject
+cd sampleProject
+npm start # (and then press i to start the ios emulator), or do:
+npm run io # (scan the QE code to actually open it on your iPhone)s
