09d97ce1b31a6d12c8b9517dad8074646cde546c
Karlijn Zaanen <karlijn_zaanen@live.nl>
2024-08-07 13:54:33 +0200

    First RN Component: Adding a footer

---
+++ react_native/module1/script_to_run_firstRNComponent.sh
@@ -0,0 +1,12 @@
+npx create-expo-app@latest --template blank firstRNComponent
+cd firstRNComponent
+npx expo start
+
+
+# This works for the blanco app, now try copying the starter code from coursera into this blank structure:
+# Don't copy the package.json file from coursera, just use the one that's generated in the blank project
+# Then again just do:
+cd firstRNComponent 
+npx expo start
+# And that works! 
+# If that doesn't work (error 115), then force quit the simulator and try again (some update will be installed)
