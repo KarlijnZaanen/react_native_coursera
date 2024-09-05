5eac060d0ba584ea370cb58664bf760786143a4a
Karlijn Zaanen <karlijn_zaanen@live.nl>
2024-09-05 13:19:55 +0200

    App with drawer navigation

---
+++ react_native/module4/appWithDrawerNav/script_to_install_drawer_nav_packages.sh
@@ -0,0 +1,9 @@
+cd appWithTabNav
+npm install @react-navigation/native
+npx expo install react-native-screens react-native-safe-area-context
+npm install @react-navigation/native-stack
+
+# Specifically for drawer nav:
+npm install @react-navigation/drawer
+npx expo install react-native-gesture-handler react-native-reanimated
+
